064ca2181e15277605876eab8f01f20f1a666aeb
Jacob Downey <jacobdowney22@gmail.com>
2020-12-18 18:34:31 -0800

    more numberS

--- helpers.py
+++ helpers.py
@@ -80,7 +80,22 @@ def scorePredictions(predictions, answers, percentError):
     groupings = {}
     i = 0.0
     diffs = [abs(predictions[i] - answers[i]) for i in range(0, len(answers))]
-    max_diffs = max(diffs) + gaps
+    diffs.sort()
+
+    median = diffs[int(len(diffs) / 2)]
+    if len(diffs) % 2 != 0:
+        median = round((diffs[int(len(diffs)/2)] + diffs[int((len(diffs)/2))+1]) / 2.0, 3)
+    print('Median Mean Absolute Error:', median)
+    mean = sum(diffs) / len(diffs)
+    print('Average Mean Absolute Error:', sum(diffs) / len(diffs))
+    std_dev = sum([(x*10000) * (x*10000) for x in diffs])
+    std_dev = math.sqrt((1.0 / len(diffs)) * std_dev) / 10000.0
+    print('Standard Deviation:', std_dev)
+    indexOf95 = int(0.95 * len(diffs))
+    prnt = 'We can say with 95% certainty that prediction x is within x -'
+    print(prnt, round(diffs[indexOf95], 4), 'and x +', round(diffs[indexOf95], 4))
+
+    max_diffs = diffs[-1] + gaps
     while i < max_diffs:
         groupings[round(i, 3)] = 0
         i += gaps
@@ -102,57 +117,3 @@ def scorePredictions(predictions, answers, percentError):
     plt.ylabel('Range of Mean Abolsute Error')
     plt.title('Range of Accuracy of Predictions by Model')
     plt.show()
-
-##### Problem 3: Comparative Studies
-# Please write a function to calculate the confusion matrix for the prediction
-# results of a classifier. The function should take the form:
-# def func_calConfusionMatrix(predY, trueY) where predY is the vector of the
-# predicted labels and trueY is the vector of true labels. This function should
-# return accuracy, per-class precision, and per-class recall rate.
-# NOTE: these are meant for normal pyhton lists not np.arrays
-def accuracies(predY, trueY):
-    if len(predY) != len(trueY):
-        print('length of predY and trueY must be equal')
-        return -1, [], []
-
-    # Calcualte accuracy. Basically (TP+TN) / TOTAL
-    accuracy025 = 0
-    accuracy05 = 0
-    accuracy10 = 0
-    accuracy25 = 0
-    for i in range(0, len(trueY)):
-        diff = abs(predY[i] - trueY[i])
-        if diff < 0.025:
-            accuracy025 += 1
-        if diff < 0.05:
-            accuracy05 += 1
-        if diff < 0.1:
-            accuracy10 += 1
-        if diff < 0.25:
-            accuracy25 += 1
-    accuracy025 = float(accuracy025) / float(len(trueY))
-    accuracy05 = float(accuracy05) / float(len(trueY))
-    accuracy10 = float(accuracy10) / float(len(trueY))
-    accuracy25 = float(accuracy25) / float(len(trueY))
-
-    return [accuracy025, accuracy05, accuracy10, accuracy25]
-
-    # # for each type compute precision and recall rates
-    # precision = []
-    # recall = []
-    # for i in range(0, len(types)):
-    #     type = types[i]
-    #     true_positive = 0
-    #     false_positive = 0
-    #     false_negative = 0
-    #     for i in range(0, len(trueY)):
-    #         if type == trueY[i] and predY[i] == trueY[i]:
-    #             true_positive += 1
-    #         if type == trueY[i] and predY[i] != trueY[i]:
-    #             false_negative += 1
-    #         if type == predY[i] and predY[i] != trueY[i]:
-    #             false_positive += 1
-    #     precision.append(float(true_positive) / float(true_positive + false_positive))
-    #     recall.append(float(true_positive) / float(true_positive + false_negative))
-    #
-    # return accuracy, precision, recall
